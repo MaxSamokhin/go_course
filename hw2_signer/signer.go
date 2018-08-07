4c48bdd87d08695085ea8d3ad70a7bb63eece1d0
MaxSamokhin <samokhinmax@gmail.com>
2018-08-07 23:50:06 +0300

    finish lesson 2

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,118 +1,139 @@
 package main
 
 import (
-	"crypto/md5"
 	"fmt"
-	"hash/crc32"
 	"sort"
 	"strconv"
 	"strings"
-	"sync/atomic"
-	"time"
+	"sync"
 )
 
-type job func(in, out chan interface{})
+func ExecutePipeline(jobs ...job) {
 
-const (
-	MaxInputDataLen = 100
-)
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
 
-var (
-	dataSignerOverheat uint32 = 0
-	DataSignerSalt            = ""
-)
+	in := make(chan interface{}, 1)
 
-var OverheatLock = func() {
-	for {
-		if swapped := atomic.CompareAndSwapUint32(&dataSignerOverheat, 0, 1); !swapped {
-			fmt.Println("OverheatLock happend")
-			time.Sleep(time.Second)
-		} else {
-			break
-		}
+	for _, currentJob := range jobs {
+		wg.Add(1)
+		out := make(chan interface{}, 1)
+
+		go func(currentJob job, in, out chan interface{}) {
+			defer wg.Done()
+			defer close(out)
+
+			currentJob(in, out)
+		}(currentJob, in, out)
+
+		in = out
 	}
 }
 
-var OverheatUnlock = func() {
-	for {
-		if swapped := atomic.CompareAndSwapUint32(&dataSignerOverheat, 1, 0); !swapped {
-			fmt.Println("OverheatUnlock happend")
-			time.Sleep(time.Second)
-		} else {
-			break
-		}
-	}
-}
-
-var DataSignerMd5 = func(data string) string {
-	OverheatLock()
-	defer OverheatUnlock()
-	data += DataSignerSalt
-	dataHash := fmt.Sprintf("%x", md5.Sum([]byte(data)))
-	time.Sleep(10 * time.Millisecond)
-	return dataHash
-}
-
-var DataSignerCrc32 = func(data string) string {
-	data += DataSignerSalt
-	crcH := crc32.ChecksumIEEE([]byte(data))
-	dataHash := strconv.FormatUint(uint64(crcH), 10)
-	time.Sleep(time.Second)
-	return dataHash
-}
-
-func ExecutePipeline() {
-	singleHash1 := SingleHash("0")
-	multiHash1 := MultiHash(singleHash1)
-
-	singleHash2 := SingleHash("1")
-	multiHash2 := MultiHash(singleHash2)
-
-	CombineResults(multiHash1, multiHash2)
-}
-
-func CombineResults(a string, b string) {
+func CombineResults(in, out chan interface{}) {
 	var arr []string
-	arr = append(arr, a, b)
-	sort.Strings(arr)
 
-	fmt.Println("CombineResults " + strings.Join(arr, "_"))
+	for elem := range in {
+		stringElem, ok := elem.(string)
+		if !ok {
+			fmt.Println("[ERROR]: failed convert data to int")
+			return
+		}
+
+		arr = append(arr, stringElem)
+	}
+
+	sort.Strings(arr)
+	out <- strings.Join(arr, "_")
 }
 
-func SingleHash(data string) string {
+func getSingleHash(data string, wg *sync.WaitGroup, wgInside *sync.WaitGroup, mu *sync.Mutex, out chan interface{}) {
+	defer wg.Done()
 	fmt.Println(data + " SingleHash data " + data)
+	var dataCrc32Md5 string
 
-	dataMd5 := DataSignerMd5(data)
-	fmt.Println(data + " SingleHash md5(data) " + dataMd5)
+	wgInside.Add(1)
 
-	dataCrc32Md5 := DataSignerCrc32(dataMd5)
-	fmt.Println(data + " SingleHash crc32(md5(data)) " + dataCrc32Md5)
+	go func(data string) {
+		defer wgInside.Done()
+
+		mu.Lock()
+		dataMd5 := DataSignerMd5(data)
+		mu.Unlock()
+		fmt.Println(data + " SingleHash md5(data) " + dataMd5)
+		dataCrc32Md5 = DataSignerCrc32(dataMd5)
+		fmt.Println(data + " SingleHash crc32(md5(data)) " + dataCrc32Md5)
+
+	}(data)
 
 	dataCrc32 := DataSignerCrc32(data)
 	fmt.Println(data + " SingleHash crc32(data) " + dataCrc32)
 
+	wgInside.Wait()
+
 	dataResult := dataCrc32 + "~" + dataCrc32Md5
 	fmt.Println(data + " SingleHash result " + dataResult)
 
-	return dataResult;
+	out <- dataResult
 }
 
-func MultiHash(data string) string {
-	var arrayHash []string
+func SingleHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
+
+	for input := range in {
+
+		wg.Add(1)
+		numberInput, ok := input.(int)
+		if !ok {
+			fmt.Println("[ERROR]: failed convert data to int")
+			return
+		}
+
+		wgInside := &sync.WaitGroup{}
+		go getSingleHash(strconv.Itoa(numberInput), wg, wgInside, mu, out)
+	}
+
+	wg.Wait()
+}
+
+func getMultiHash(data string, wg *sync.WaitGroup, out chan interface{}) {
+	defer wg.Done()
+
+	wgInside := &sync.WaitGroup{}
+	var arrayHash = make([]string, 6, 6)
 
 	for i := 0; i < 6; i++ {
-		newData := strconv.Itoa(i) + data
-		crcParam := DataSignerCrc32(newData)
+		wgInside.Add(1)
 
-		arrayHash = append(arrayHash, crcParam)
-		fmt.Println(data + " MultiHash: crc32(th+step1)) " + strconv.Itoa(i) + " " + crcParam)
+		go func(i int) {
+			defer wgInside.Done()
+			crcParam := DataSignerCrc32(strconv.Itoa(i) + data)
+			arrayHash[i] = crcParam
+		}(i)
 	}
-	result := strings.Join(arrayHash, "")
-	fmt.Println(data + " MultiHash result " + result)
 
-	return strings.Join(arrayHash, "")
+	wgInside.Wait()
+
+	result := strings.Join(arrayHash, "")
+	fmt.Println(data + " MultiHash: crc32(th+step1)) " + result)
+
+	out <- result
 }
 
-func main() {
-	ExecutePipeline()
+func MultiHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	for input := range in {
+
+		inputString, ok := input.(string)
+		if !ok {
+			fmt.Println("[ERROR]: failed convert data to int")
+			return
+		}
+		wg.Add(1)
+
+		go getMultiHash(inputString, wg, out)
+	}
+
+	wg.Wait()
 }
